57c46149570ede0d21ea8ff433fa219b904d2610
Epic <admin@itzepic.net>
2020-07-04 18:58:35 +0200

    Add bans to datadriver

--- datadriver/models.py
+++ datadriver/models.py
@@ -3,7 +3,7 @@ Created by Epic at 7/4/20
 """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, BIGINT, Integer, DateTime
+from sqlalchemy import Column, BIGINT, Integer, DateTime, String
 
 Base = declarative_base()
 
@@ -17,3 +17,13 @@ class User(Base):
 
     def __repr__(self):
         return f"<User(id={self.id}, messages=f{self.messages}, join_date=f{self.join_date})>"
+
+
+class Ban(Base):
+    __tablename__ = "bans"
+
+    id = Column(BIGINT, primary_key=True, unique=True)
+    banned_at = Column(DateTime)
+    expires_at = Column(DateTime)
+
+    reason = Column(String)
